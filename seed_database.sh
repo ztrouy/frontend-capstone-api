637185f94f9da36f61ba6f448bd56606a9d6474d
Zachary Trouy <ztrouy@gmail.com>
2024-08-28 17:11:49 -0500

    Added commands to load seed data for Games, Genres, Groups, Platforms, Game Genres, Game Platforms, User Games, and User Groups

--- seed_database.sh
+++ seed_database.sh
@@ -7,4 +7,12 @@ python manage.py makemigrations gamegaugeapi
 python manage.py migrate gamegaugeapi
 python manage.py loaddata users
 python manage.py loaddata tokens
+python manage.py loaddata genres
+python manage.py loaddata platforms
+python manage.py loaddata games
+python manage.py loaddata groups
+python manage.py loaddata game_genres
+python manage.py loaddata game_platforms
+python manage.py loaddata user_games
+python manage.py loaddata user_groups
 
