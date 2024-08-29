094356de4b0b5d7597f12a5874b42feabd6a120a
Zachary Trouy <ztrouy@gmail.com>
2024-08-29 12:56:30 -0500

    Created file, and wrote initial serializers, and views for registration, login, and authentication

---
+++ gamegaugeapi/views/users.py
@@ -0,0 +1,66 @@
+from rest_framework import viewsets, status, permissions, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "password", "username"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+
+class UserViewSet(viewsets.ViewSet):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+
+    @action(detail=False, methods=["post"], url_path="register")
+    def register_account(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(
+                username=serializer.validated_data["username"],
+                email=serializer.validated_data["email"],
+                first_name="",
+                last_name="",
+                password=serializer.validated_data["password"]
+            )
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["post"], url_path="login")
+    def user_login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token = Token.objects.get(user=user)
+            return Response({"token": token.key, "valid": True}, status=status.HTTP_200_OK)
+
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["post"], url_path="me")
+    def auth_user(self, request):
+        token_key = request.data.get("token")
+
+        try:
+            token = Token.objects.get(key=token_key)
+            user = User.objects.get(id=token.user_id)
+            serializer = UserSerializer(user, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Token.DoesNotExist:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
