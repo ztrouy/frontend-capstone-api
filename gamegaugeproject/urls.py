8faa395eca6cbc9b44cfd8ed5fc1364e9648b662
Zachary Trouy <ztrouy@gmail.com>
2024-08-29 14:29:59 -0500

    Added routing for Games View Set

--- gamegaugeproject/urls.py
+++ gamegaugeproject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from gamegaugeapi.views import UserViewSet, GenreViewSet, PlatformViewSet
+from gamegaugeapi.views import UserViewSet, GameViewSet, GenreViewSet, PlatformViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+router.register(r"games", GameViewSet, "game")
 router.register(r"genres", GenreViewSet, "genre")
 router.register(r"platforms", PlatformViewSet, "platform")
 
