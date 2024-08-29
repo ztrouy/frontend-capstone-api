1431bed45e2cf5dc7ed65dfa26d86b682f2a0769
Zachary Trouy <ztrouy@gmail.com>
2024-08-29 15:08:10 -0500

    Wrote serializer that includes Games and Members, and wrote retrieve view

--- gamegaugeapi/views/groups.py
+++ gamegaugeapi/views/groups.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, status, serializers
 from rest_framework.response import Response
 from gamegaugeapi.models import Group
+from .users import UserSerializer
+from .games import GameSerializer
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -19,8 +21,39 @@ class GroupSerializer(serializers.ModelSerializer):
         return obj.members.count()
 
 
+class GroupDetailedSerializer(serializers.ModelSerializer):
+    isMember = serializers.SerializerMethodField()
+    memberCount = serializers.SerializerMethodField()
+    games = serializers.SerializerMethodField()
+    members = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Group
+        fields = ["id", "name", "members", "memberCount", "games", "isMember"]
+
+    def get_isMember(self, obj):
+        user = self.context["request"].user
+        return obj.members.filter(id=user.id).exists()
+
+    def get_memberCount(self, obj):
+        return obj.members.count()
+
+    def get_games(self, obj):
+        games = obj.get_common_games()
+        return GameSerializer(games, many=True, context={"request": self.context["request"]}).data
+
+
 class GroupViewSet(viewsets.ViewSet):
     def list(self, request):
         groups = Group.objects.all()
         serializer = GroupSerializer(groups, many=True, context={"request": request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        try:
+            group = Group.objects.get(pk=pk)
+            serializer = GroupDetailedSerializer(group, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+        except Group.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
