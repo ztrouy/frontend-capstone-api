6b3d6c185e8e4f48f49c6c8935c8409f3a888e7f
Zachary Trouy <ztrouy@gmail.com>
2024-09-03 14:26:18 -0500

    Removed unused imports

--- gamegaugeapi/views/groups.py
+++ gamegaugeapi/views/groups.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status, serializers
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from gamegaugeapi.models import Group, UserGroup, Game
+from gamegaugeapi.models import Group
 from .users import UserSerializer
 from .games import GameSerializer
 
@@ -48,6 +48,7 @@ class GroupUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ["name"]
+
 
 class GroupViewSet(viewsets.ViewSet):
     def list(self, request):
